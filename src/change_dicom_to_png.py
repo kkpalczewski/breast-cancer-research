8c77541e8a48e03332291e33f2036525500df41a
kkpalczewski <firstcalmly@gmail.com>
2019-11-17 13:02:07 +0100

    Add creating new metadata after PNG conversion in change_dicom_to_png

--- src/change_dicom_to_png.py
+++ src/change_dicom_to_png.py
@@ -3,6 +3,8 @@ import subprocess
 import shutil
 import time
 import argparse
+import pandas as pd
+import os
 
 def create_dir(dirName):
     """
@@ -61,6 +63,30 @@ def change_dicom_to_png(source_folder, dest_folder):
         if i % 100 == 0:
             print("Iteration: {}".format(i))
 
+def change_extension_in_csv(source_csv, destination_csv):
+    source_csv_file = pd.read_csv(source_csv)
+
+    fields_to_change = ['image file path', 'cropped image file path', 'ROI mask file path']
+
+    try:
+        first_record = source_csv_file.iloc[0]
+        for field in fields_to_change:
+            print("Field to change: {0:30} , sample value: {1}".format(field, first_record[field]))
+    except KeyError as err:
+        print("Change colmn name, which should be converted. ERROR: {}".format(err))
+        return err
+
+    for id, row in source_csv_file.iterrows():
+        for field in fields_to_change:
+            source_csv_file.at[id, field] = source_csv_file.iloc[id][field].split(".dcm")[0] + ".png"
+
+    try:
+        source_csv_file.to_csv(destination_csv)
+        print("[SUCCESS] New metadata saved in path: {}".format(destination_csv))
+    except BaseException as err:
+        print("[FAILURE] of saving {}, due to exception: {}".format(destination_csv, err))
+
+
 def main():
     """
     Create PNG directories from cmd
@@ -69,8 +95,30 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('source_folder', help='source_folder')
     parser.add_argument('destination_folder', help='destination_folder')
+    parser.add_argument('source_metadata_folder', help='source metadata folder (default:None)', const=None)
+    parser.add_argument('destination_metadata_folder', help='destination metadata folder (default:None)', const=None)
+
     args = parser.parse_args()
+
     change_dicom_to_png(args.source_folder, args.destination_folder)
+
+    if args.source_metadata_folder != None and args.destination_metadata_folder != None:
+        source_metadata = [
+            os.path.join(args.source_metadata_folder, "calc_case_description_test_set.csv"),
+            os.path.join(args.source_metadata_folder, "calc_case_description_train_set.csv"),
+            os.path.join(args.source_metadata_folder, "mass_case_description_test_set.csv"),
+            os.path.join(args.source_metadata_folder, "mass_case_description_train_set.csv"),
+        ]
+        destination_metadata = [
+            os.path.join(args.destination_metadata_folder, "calc_case_description_test_set_PNG.csv"),
+            os.path.join(args.destination_metadata_folder, "calc_case_description_train_set_PNG.csv"),
+            os.path.join(args.destination_metadata_folder, "mass_case_description_test_set_PNG.csv"),
+            os.path.join(args.destination_metadata_folder, "mass_case_description_train_set_PNG.csv"),
+        ]
+
+        for s, d in zip(source_metadata, destination_metadata):
+            change_extension_in_csv(s, d)
+
 
 if __name__=="__main__":
     main()
